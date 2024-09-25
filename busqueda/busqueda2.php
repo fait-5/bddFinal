a3de11707b69032f2c010bfb621cfb607ba661ad
Esteban Gómez Benítez <esgomez@unal.edu.co>
2024-09-25 13:25:32 -0500

    Añadí archivos manualmente jeeje
    Cambios en el archivo "búsqueda2.php" dada la consulta que nos dieron

--- busqueda/busqueda2.php
+++ busqueda/busqueda2.php
@@ -6,9 +6,10 @@ include "../includes/header.php";
 <h1 class="mt-3">Búsqueda 2</h1>
 
 <p class="mt-3">
-    Dos números enteros n1 y n2, n1 ≥ 0, n2 > n1. Se debe mostrar el nit y el 
-    nombre de todas las empresas que han revisado entre n1 y n2 proyectos
-    (intervalo cerrado [n1, n2]).
+    El carnet de un ASESOR. Se debe mostrar, para el cliente o usuario de 
+    mayor edad (año de nacimiento) asignado a ese ASESOR,
+    todos los datos de todas las RESERVAS que ese CLIENTE
+    canceló (en caso de empates, usted decide cómo proceder).
 </p>
 
 <!-- FORMULARIO. Cambiar los campos de acuerdo a su trabajo -->
@@ -18,13 +19,8 @@ include "../includes/header.php";
     <form action="busqueda2.php" method="post" class="form-group">
 
         <div class="mb-3">
-            <label for="numero1" class="form-label">Numero 1</label>
+            <label for="numero1" class="form-label">Carnet Asesor</label>
             <input type="number" class="form-control" id="numero1" name="numero1" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="numero2" class="form-label">Numero 2</label>
-            <input type="number" class="form-control" id="numero2" name="numero2" required>
         </div>
 
         <button type="submit" class="btn btn-primary">Buscar</button>
@@ -40,11 +36,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'):
     // Crear conexión con la BD
     require('../config/conexion.php');
 
-    $numero1 = $_POST["numero1"];
-    $numero2 = $_POST["numero2"];
+    $numero1 = $_POST["carnet"];
+    //$numero2 = $_POST["id_reserva"];
+    //$numero2 = $_POST["fecha_reserva"];
+    //$numero2 = $_POST["fecha_cancelacion"];
+    //$numero2 = $_POST["valor"];
 
     // Query SQL a la BD -> Crearla acá (No está completada, cambiarla a su contexto y a su analogía)
-    $query = "SELECT nit, nombre FROM empresa";
+    $query = "SELECT carnet, (select * from reserva)
+        from asesor INNER JOIN cliente ON carnet = asesor
+        INNER JOIN reserva ON fecha_cancelacion = documento_identidad
+        GROUP BY asesor
+        HAVING MIN(año_nacimiento)";
 
     // Ejecutar la consulta
     $resultadoB2 = mysqli_query($conn, $query) or die(mysqli_error($conn));
@@ -63,8 +66,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'):
         <!-- Títulos de la tabla, cambiarlos -->
         <thead class="table-dark">
             <tr>
-                <th scope="col" class="text-center">Cédula</th>
-                <th scope="col" class="text-center">Celular</th>
+                <th scope="col" class="text-center">Carnet Asesor</th>
+                <th scope="col" class="text-center">id_reserva</th>
+                <th scope="col" class="text-center">fecha_reserva</th>
+                <th scope="col" class="text-center">fecha_cancelacion</th>
+                <th scope="col" class="text-center">valor</th>
             </tr>
         </thead>
 
@@ -78,8 +84,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'):
             <!-- Fila que se generará -->
             <tr>
                 <!-- Cada una de las columnas, con su valor correspondiente -->
-                <td class="text-center"><?= $fila["cedula"]; ?></td>
-                <td class="text-center"><?= $fila["celular"]; ?></td>
+                <td class="text-center"><?= $fila["carnet"]; ?></td>
+                <td class="text-center"><?= $fila["id_reserva"]; ?></td>
+                <td class="text-center"><?= $fila["fecha_reserva"]; ?></td>
+                <td class="text-center"><?= $fila["fecha_cancelacion"]; ?></td>
+                <td class="text-center"><?= $fila["valor"]; ?></td>
             </tr>
 
             <?php
